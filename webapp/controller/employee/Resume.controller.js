80cb94225051fa58489822735d734fb488e38cea
Joanna Krzyzanowska <joanna.krzyzanowska@o2.pl>
2022-08-12 12:56:13 +0000

    step 8: navigate with flip transition

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -2,26 +2,15 @@ sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController"
 ], function (BaseController) {
 	"use strict";
-	return BaseController.extend("sap.ui.demo.nav.controller.employee.Employee", {
+	return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
 		onInit: function () {
 			var oRouter = this.getRouter();
-			oRouter.getRoute("employee").attachMatched(this._onRouteMatched, this);
-			// Hint: we don't want to do it this way
-			/*
-			oRouter.attachRouteMatched(function (oEvent){
-				var sRouteName, oArgs, oView;
-				sRouteName = oEvent.getParameter("name");
-				if (sRouteName === "employee"){
-					this._onRouteMatched(oEvent);
-				}
-			}, this);
-			*/
+			oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched : function (oEvent) {
 			var oArgs, oView;
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
-
 			oView.bindElement({
 				path : "/Employees(" + oArgs.employeeId + ")",
 				events : {
@@ -40,13 +29,6 @@ sap.ui.define([
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
-		},
-		onShowResume : function (oEvent) {
-			var oCtx = this.getView().getElementBinding().getBoundContext();
-
-			this.getRouter().navTo("employeeResume", {
-				employeeId : oCtx.getProperty("EmployeeID")
-			});
 		}
 	});
 });
