d51c095d69a34ef95caabc98aa4b91490eacc431
Joanna Krzyzanowska <joanna.krzyzanowska@o2.pl>
2022-08-24 11:11:37 +0000

    step 9: allow bookmarktable tabs with optional query parameters

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -1,14 +1,19 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+
+], function (BaseController, JSONModel) {
 	"use strict";
+
+    var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
 		onInit: function () {
 			var oRouter = this.getRouter();
 			oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
+            this.getView().setModel(new JSONModel(), "view");
 		},
 		_onRouteMatched : function (oEvent) {
-			var oArgs, oView;
+			var oArgs, oView, oQuery;
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
 			oView.bindElement({
@@ -23,12 +28,35 @@ sap.ui.define([
 					}
 				}
 			});
+
+            oQuery = oArgs["?query"];
+			if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1){
+				oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+			} else {
+				// the default query param should be visible at all time
+				this.getRouter().navTo("employeeResume", {
+					employeeId: oArgs.employeeId,
+					"?query": {
+						tab: _aValidTabKeys[0]
+					}
+				}, true /*no history*/);
+			}
 		},
 		_onBindingChange : function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
+		},
+
+        onTabSelect: function (oEvent){
+			var oCtx = this.getView().getBindingContext();
+			this.getRouter().navTo("employeeResume", {
+				employeeId: oCtx.getProperty("EmployeeID"),
+				"?query": {
+					tab: oEvent.getParameter("selectedKey")
+				}
+			}, true /*without history*/);
 		}
 	});
 });
