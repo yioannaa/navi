527711777e6d4b4a6ac75552e4af9125d07c28d6
Joanna Krzyzanowska <joanna.krzyzanowska@o2.pl>
2022-09-01 09:48:26 +0000

    step 11: Assign Multiple Targets

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -13,6 +13,9 @@ sap.ui.define([
 		},
         onNavToEmployees : function (){
 			this.getRouter().navTo("employeeList");
+		},
+        onNavToEmployeeOverview : function ()  {
+			this.getRouter().navTo("employeeOverview");
 		}
     });
  
